e49f21e32d46fb76c8c8b619e98a69d1fcf3aae1
GabrielCM08 <gabriel.camposm@hotmail.com>
2021-10-14 21:32:12 -0300

    finalized and styled form/cad

--- ediaristas_workshop/ediaristas_workshop/settings.py
+++ ediaristas_workshop/ediaristas_workshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-td@(!a&whft#)%5t*r4nnw6hi23(1dgr_hy4pdi!*zj@@!$z)9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'easy_mask',
     'web'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
